36cf31f08844da032979c350eb658a116d2e232f
Martin Lehmann <martin@folio.no>
2021-02-08 21:20:03 +0100

    readline

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,6 @@
 #!/usr/bin/env node
 import * as crypto from 'crypto';
-
-const input = `
-gke-prod-pool-87102dd3-kjl7   Ready                         <none>   4s      v1.16.15-gke.6000
-gke-prod-pool-87102dd3-y5bp   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-l7sr   Ready                         <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-sp57   Ready                         <none>   3m2s    v1.16.15-gke.6000
-gke-prod-pool-87102dd3-z4zf   Ready                         <none>   9m33s   v1.16.15-gke.6000
-gke-prod-pool-87102dd3-kxrz   Ready,SchedulingDisabled      <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-kxrz   Ready,SchedulingDisabled      <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-o6h2   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-o6h2   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-l7sr   Ready                         <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-ks2p   Ready                         <none>   13m     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-kjl7   Ready                         <none>   30s     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-y5bp   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-ro1v   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-z4zf   Ready                         <none>   10m     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-kxrz   Ready,SchedulingDisabled      <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-ro1v   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-t98g   Ready                         <none>   6m33s   v1.16.15-gke.6000
-gke-prod-pool-87102dd3-kjl7   Ready                         <none>   61s     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-y5bp   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-l7sr   Ready                         <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-sp57   Ready                         <none>   4m2s    v1.16.15-gke.6000
-gke-prod-pool-87102dd3-z4zf   Ready                         <none>   10m     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-kxrz   NotReady,SchedulingDisabled   <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-kxrz   NotReady,SchedulingDisabled   <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-kxrz   NotReady,SchedulingDisabled   <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-o6h2   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-o6h2   Ready                         <none>   18h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-l7sr   Ready                         <none>   17h     v1.15.12-gke.6002
-gke-prod-pool-87102dd3-ks2p   Ready                         <none>   14m     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-kjl7   Ready                         <none>   91s     v1.16.15-gke.6000
-gke-prod-pool-87102dd3-y5bp   Ready                         <none>   18h     v1.15.12-gke.6002
-`
-  .trim()
-  .split('\n');
+import * as readline from 'readline';
 
 const colorPrefixes = {
   black: '\x1b[30m',
@@ -64,7 +28,7 @@ function colorPrefix(word: string): string {
   return colorPrefixes[colorKey];
 }
 
-function colorLine(line: string) {
+function colorLine(line: string): string[] {
   const words = line.split(/(\s+)/);
   const outputs = [];
 
@@ -78,9 +42,14 @@ function colorLine(line: string) {
     outputs.push(word);
   }
 
-  console.log(...outputs);
+  return outputs;
 }
 
-for (const line of input) {
-  colorLine(line);
-}
+readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on('line', (line) => {
+    console.log(...colorLine(line));
+  });
